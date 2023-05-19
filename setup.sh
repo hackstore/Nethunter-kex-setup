c50f2512c19c0c6ce7759fa668ef923eaeea2566
hackstore <122078666+hackstore@users.noreply.github.com>
2023-05-19 09:53:04 +0600

    setup.sh
    nh -r

--- setup.sh
+++ setup.sh
@@ -4,27 +4,13 @@ echo -e $grn"Installing Required Packages By ProHunter"
 echo -e $grn
 sudo apt update && sudo apt full-upgrade -y
 echo -e $grn
-sudo apt install python3 php -y
+sudo apt autoremove -y
+echo -e $grn
+sudo apt install python3 php git wget -y
 echo -e $grn
 sudo apt install udisks2 -y
 echo -e $grn
-sudo apt install xfce4 -y
-echo -e $grn
-sudo apt install xfce4-whiskermenu-plugin -y
-echo -e $grn
-sudo apt install qterminal -y
-echo -e $grn
-sudo apt install dbus-x11 -y
-echo -e $grn
-sudo apt install firefox-esr -y
-echo -e $grn
-sudo apt install tigervnc-standalone-server -y
-echo -e $grn
-sudo apt install kali-themes -y
-echo -e $grn
-sudo apt install net-tools -y
-echo -e $grn
-sudo apt install ftp ssh -y
+sudo apt install xfce4 xfce4-whiskermenu-plugin qterminal dbus-x11 firefox-esr tigervnc-standalone-server kali-themes ssh ftp net-tools -y
 echo -e $grn
 chmod +x /usr/bin/vncst*
 echo -e $grn
