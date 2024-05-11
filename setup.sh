1e9b9744c0b9f5d8c5add8c991b464539056815c
hackstore <122078666+hackstore@users.noreply.github.com>
2024-05-11 15:20:02 +0600

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,37 @@
 clear
-echo -e $grn
-echo -e $grn"Installing Required Packages By ProHunter"
-echo -e $grn
+    printf "${blue}##################################################\n"
+    printf "${blue}##                                              ##\n"
+    printf "${blue}##  88      a8P         db        88        88  ##\n"
+    printf "${blue}##  88    .88'         d88b       88        88  ##\n"
+    printf "${blue}##  88   88'          d8''8b      88        88  ##\n"
+    printf "${blue}##  88 d88           d8'  '8b     88        88  ##\n"
+    printf "${blue}##  8888'88.        d8YaaaaY8b    88        88  ##\n"
+    printf "${blue}##  88P   Y8b      d8''''''''8b   88        88  ##\n"
+    printf "${blue}##  88     '88.   d8'        '8b  88        88  ##\n"
+    printf "${blue}##  88       Y8b d8'          '8b 888888888 88  ##\n"
+    printf "${blue}##                                              ##\n"
+    printf "${blue}################# By ProHunter ###################${reset}\n\n"
+
+
+
+##################################
+##              Main            ##
+
+# Add some colours
+red='\033[1;31m'
+green='\033[1;32m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+light_cyan='\033[1;96m'
+reset='\033[0m'
+
+EXTRA_ARGS=""
+if [[ ! -z $1 ]]; then
+    EXTRA_ARGS=$1
+    if [[ $EXTRA_ARGS != "--no-check-certificate" ]]; then
+        EXTRA_ARGS=""
+ if
+if
 sudo apt update && sudo apt full-upgrade -y
 echo -e $grn
 sudo apt install net-tools firefox-esr kali-themes kali-desktop-xfce dbus-x11 tigervnc-standalone-server -y
