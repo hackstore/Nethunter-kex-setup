e3216a8dd4b4468f4831ce5ae59dc15ed8d30c44
hackstore <122078666+hackstore@users.noreply.github.com>
2024-05-11 15:38:44 +0600

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+
 clear
     printf "${blue}##################################################\n"
     printf "${blue}##                                              ##\n"
@@ -11,27 +12,6 @@ clear
     printf "${blue}##  88       Y8b d8'          '8b 888888888 88  ##\n"
     printf "${blue}##                                              ##\n"
     printf "${blue}################# By ProHunter ###################${reset}\n\n"
-
-
-
-##################################
-##              Main            ##
-
-# Add some colours
-red='\033[1;31m'
-green='\033[1;32m'
-yellow='\033[1;33m'
-blue='\033[1;34m'
-light_cyan='\033[1;96m'
-reset='\033[0m'
-
-EXTRA_ARGS=""
-if [[ ! -z $1 ]]; then
-    EXTRA_ARGS=$1
-    if [[ $EXTRA_ARGS != "--no-check-certificate" ]]; then
-        EXTRA_ARGS=""
- if
-if
 sudo apt update && sudo apt full-upgrade -y
 echo -e $grn
 sudo apt install net-tools firefox-esr kali-themes kali-desktop-xfce dbus-x11 tigervnc-standalone-server -y
